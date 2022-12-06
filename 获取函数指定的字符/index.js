33ea0974944eaf88e1614b7952bdb7a02d6f5892
星河踏月来 <84435225+Amor-ai@users.noreply.github.com>
2022-12-06 19:07:00 +0800

    Create index.js

---
+++ 获取函数指定的字符/index.js
@@ -0,0 +1,6 @@
+//获取函数指定的字符
+let myStr = 'I am a "double quoted" string inside "double quotes".';
+const iliveyou = "iliveyou";
+//myStr=iliveyou[2];/*获取第三个字符*/ 
+myStr = iliveyou[iliveyou.length - 1]/*获取最后一个字符*/
+console.log(myStr);
