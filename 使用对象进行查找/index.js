7f01dd0fcd2741296503f0e3f9059e32dae8373e
星河踏月来 <84435225+Amor-ai@users.noreply.github.com>
2022-12-06 19:09:29 +0800

    Create index.js

---
+++ 使用对象进行查找/index.js
@@ -0,0 +1,15 @@
+// 设置
+function phoneticLookup(val) {
+    let result = "";
+    var lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank"
+    }
+    result = lookup[val];
+    return result;
+}
+console.log(phoneticLookup("charlie"));
