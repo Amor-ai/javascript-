d388a26b4482bd6cb336358eb382b7652b1a93f0
星河踏月来 <84435225+Amor-ai@users.noreply.github.com>
2022-12-08 11:07:04 +0800

    Create index.js

---
+++ 操作复杂对象/index.js
@@ -0,0 +1,22 @@
+const myMusic = [  //数组 可以操作字符串（strings）、数字（numbers）、布尔值（booleans）、数组（arrays）、函数（functions）和对象（objects） 以及任意组合
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "hello world",
+      "title": "I live you",
+      "release_year":2022,
+      "formats" : [
+        "CD",
+        "8T"
+      ]
+    }
+  ];
